475bb4eeb0b3009b8eec7ac0d1daf9b9131c0ee3
oopanpan <oopanpan@gmail.com>
2021-02-03 08:52:19 -0800

    rad

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+    Student.create(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name)
+end
